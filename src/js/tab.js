e10635f8ffe45c74a1d76672fac7e716cbfbe3db
kao4 <167847955+kao4@users.noreply.github.com>
2025-02-26 10:37:58 +0900

    ファイル名の変更や修正

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,13 +1,15 @@
 // タブ切り替えの処理
-document.querySelectorAll(".tab").forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // アクティブなタブとコンテンツを非表示に
-    document
-      .querySelectorAll(".tab.active, .tab-content.active")
-      .forEach((el) => el.classList.remove("active"));
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".tab").forEach((tab) => {
+    tab.addEventListener("click", () => {
+      // アクティブなタブとコンテンツを非表示に
+      document
+        .querySelectorAll(".tab.active, .tab-content.active")
+        .forEach((el) => el.classList.remove("active"));
 
-    // クリックされたタブとそれに対応するコンテンツを表示
-    tab.classList.add("active");
-    document.getElementById(tab.dataset.tab).classList.add("active");
+      // クリックされたタブとそれに対応するコンテンツを表示
+      tab.classList.add("active");
+      document.getElementById(tab.dataset.tab).classList.add("active");
+    });
   });
 });
